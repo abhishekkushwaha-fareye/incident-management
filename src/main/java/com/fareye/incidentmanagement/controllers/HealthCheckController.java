8a6f52346b16a60dfb50c56a63fa81229e4117a2
abhishekkushwaha-fareye <abhishek.kushwaha@fareye.co>
2024-05-17 02:12:27 +0530

    initial commit for incident management

---
+++ src/main/java/com/fareye/incidentmanagement/controllers/HealthCheckController.java
@@ -0,0 +1,11 @@
+package com.fareye.incidentmanagement.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "OK";
+    }
+}
